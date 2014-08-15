79abe6fb4562322a3f9686480f9f69b7c78266c3
Meine Toonen <toonen.meine@gmail.com>
2014-08-15 22:45:10 +0200

    printList() changed to list(PrintWriter) for API consistency

--- src/main/java/nl/meg/propertiesutility/Properties.java
+++ src/main/java/nl/meg/propertiesutility/Properties.java
@@ -13,10 +13,10 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -117,9 +117,9 @@ public class Properties {
         }
     }
     
-    public void printList(){
+    public void list(PrintWriter pw){
         for (Property property : propertyList) {
-            System.out.println(property.toString());
+            pw.println(property.toString());
         }
     }
     public enum LineType {
