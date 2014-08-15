375dbb4cf7bbf9c33b2604736ba21d98b1df544c
HJ200 <>
2014-08-15 21:04:25 +0200

    First JUnit tests

--- src/test/java/nl/meg/propertiesutility/PropertiesTest.java
+++ src/test/java/nl/meg/propertiesutility/PropertiesTest.java
@@ -5,12 +5,31 @@ import org.junit.Test;
 
 public class PropertiesTest {
 
-    @Test
-    public void iets() {
-        Properties properties = new Properties();
-        properties.setProperty("key", "value");
+    private Properties properties = new Properties();
 
+    @Test
+    public void storeSimpleKeyValue() {
+        properties.setProperty("key", "value");
         assertEquals("value", properties.getProperty("key"));
     }
-
+    
+        @Test
+    public void storeKeyValue() {
+        properties.setProperty("parent.child.key", "val");
+        assertEquals("val", properties.getProperty("parent.child.key"));
+    }
+    
+    @Test
+    public void getDefaultValue() {
+        String value = properties.getProperty("not.a.key", "mydefault");
+        assertEquals("mydefault", value);
+    }
+    
+    @Test
+    public void load() {
+        properties.load(PropertiesTest.class.getResourceAsStream("example.properties"));
+        
+        
+        
+    }
 }
