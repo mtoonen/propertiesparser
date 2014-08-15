a875d1869f4d2a505437fdfc7ebb6aa08b8e19b7
HJ200 <Gertjan@Alienwarez>
2014-08-15 21:40:48 +0200

    Added load test

--- src/test/java/nl/meg/propertiesutility/PropertiesTest.java
+++ src/test/java/nl/meg/propertiesutility/PropertiesTest.java
@@ -1,33 +1,56 @@
 package nl.meg.propertiesutility;
 
-import static junit.framework.Assert.assertEquals;
+import java.io.InputStream;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
 public class PropertiesTest {
 
-    private Properties properties = new Properties();
+    private final Properties properties = spy(new Properties());
 
+    @Before
+    public void before() {
+        doAnswer(new Answer() {
+
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                properties.setProperty("some.super.long.key", "Some text you'll never read !@#$%^&*(*");
+                properties.setProperty("parent.child", "val");
+                properties.setProperty("simplekey", "simplevalue");
+                return null;
+            }
+        }).when(properties).load(any(InputStream.class));
+    }
+    
     @Test
-    public void storeSimpleKeyValue() {
+    public void testStoreSimpleKeyValue() {
         properties.setProperty("key", "value");
         assertEquals("value", properties.getProperty("key"));
     }
-    
-        @Test
-    public void storeKeyValue() {
+
+    @Test
+    public void testStoreKeyValue() {
         properties.setProperty("parent.child.key", "val");
         assertEquals("val", properties.getProperty("parent.child.key"));
     }
-    
+
     @Test
-    public void getDefaultValue() {
+    public void testGetDefaultValue() {
         String value = properties.getProperty("not.a.key", "mydefault");
         assertEquals("mydefault", value);
     }
-    
+
     @Test
-    public void load() {
+    public void testLoad() {
         properties.load(PropertiesTest.class.getResourceAsStream("example.properties"));
+        assertEquals("Some text you'll never read !@#$%^&*(*", properties.getProperty("some.super.long.key"));
+        assertEquals("val", properties.getProperty("parent.child"));
+        assertEquals("simplevalue", properties.getProperty("simplekey"));
         
+        verify(properties, times(3)).getProperty(anyString());
     }
 }
